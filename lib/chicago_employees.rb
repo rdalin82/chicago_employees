b5ad3387580f1341abaf19c06ad4c92e371d2f3d
rdalin82 <Robert.Dalin@gmail.com>
2016-03-31 18:45:30 -0500

    updated all method

--- lib/chicago_employees.rb
+++ lib/chicago_employees.rb
@@ -3,7 +3,8 @@ require 'unirest'
 module ChicagoEmployees
   class Employee
   	def self.all
-
+  		ruby_data = []
+  		bulk_data = Unirest.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
   	end
   end
 end
